b9e8067a02ed73cb8dd87c46cc78f34fca95e0ed
DaStinkingScaliwag <gcperk20@gmail.com>
2018-02-03 17:53:18 -0500

    Began tunign a PID CenterOnGear

--- src/org/usfirst/frc/team1100/robot/commands/vision/CenterOnCube.java
+++ src/org/usfirst/frc/team1100/robot/commands/vision/CenterOnCube.java
@@ -1,28 +1,25 @@
 package org.usfirst.frc.team1100.robot.commands.vision;
 
+import org.usfirst.frc.team1100.robot.Robot;
 import org.usfirst.frc.team1100.robot.commands.drive.ChangeHeading;
 import org.usfirst.frc.team1100.robot.subsystems.vision.Limelight;
 
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class CenterOnCube {
+/**
+ *
+ */
+public class CenterOnCube extends CommandGroup {
 
-	Limelight lime;
-	
-	private boolean breaker = false; 
-	
-	//Center X of Cube
-	private double centerX = 0;
-	
-	public CenterOnCube() {
-		centerX = lime.getX();
-	}
-	
-	public void CubeCcenter() {
-		while(breaker == false)
-			new ChangeHeading(centerX*27);
-			centerX = lime.getX();
-			if(centerX <= .05 && centerX >= -.05) {
-				breaker = true;
-			}	
-	}
+    public CenterOnCube() {
+    	Limelight lime = Limelight.getInstance();
+    	if (lime.getArea() != -1) {
+    		System.err.println("Running");
+	    	double centerX = lime.getX();
+	    	double heading = 27/centerX;
+	    	SmartDashboard.putNumber("Center Heading", Robot.getAHRS().getYaw()+heading);
+			addSequential(new ChangeHeading(Robot.getAHRS().getYaw()+heading));
+    	}
+    }
 }
--- src/org/usfirst/frc/team1100/robot/commands/vision/CenterOnCubec.java
+++ src/org/usfirst/frc/team1100/robot/commands/vision/CenterOnCubec.java
@@ -1,15 +1,25 @@
 package org.usfirst.frc.team1100.robot.commands.vision;
 
-import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team1100.robot.subsystems.Drive;
+import org.usfirst.frc.team1100.robot.subsystems.vision.Limelight;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.PIDCommand;
 
 /**
  *
  */
-public class CenterOnCubec extends Command {
-
+public class CenterOnCubec extends PIDCommand {
+	private PIDController pidController = getPIDController();
     public CenterOnCubec() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    	super(.005, .0025, .04);
+    	requires(Limelight.getInstance());
+    	requires(Drive.getInstance()); 
+        setInputRange(-27, 27);
+        pidController.setOutputRange(-1, 1);
+        pidController.setContinuous();
+        pidController.setPercentTolerance(0.1);
+        setSetpoint(0);
     }
 
     // Called just before this Command runs the first time
@@ -21,8 +31,18 @@ public class CenterOnCubec extends Command {
     }
 
     // Make this return true when this Command no longer needs to run execute()
+    int countOnTarget = 0;
     protected boolean isFinished() {
-        return false;
+    	if (pidController.onTarget()) {
+    		if (countOnTarget == 5) {
+    			return true;
+    		}
+    		countOnTarget++;
+    		
+    	} else {
+    		countOnTarget = 0;
+    	}
+    	return false;
     }
 
     // Called once after isFinished returns true
@@ -33,4 +53,20 @@ public class CenterOnCubec extends Command {
     // subsystems is scheduled to run
     protected void interrupted() {
     }
+
+	@Override
+	protected double returnPIDInput() {
+		//If not detected, end
+		if (Limelight.getInstance().getArea() == -1) {
+			return 0;
+		}
+		System.err.println(Limelight.getInstance().getX());
+		return Limelight.getInstance().getX();
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+
+		Drive.getInstance().tankDrive(output, -output);
+	}
 }
