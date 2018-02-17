8424a9f83b69668c608e5747dda39369a982d814
Thor Smith <thor.smith@frc1100>
2018-02-17 14:15:34 -0500

    Adds timing to the execution of pull/shoot command
    This change adds timing to the execution of the pull
    and shoot commands.

--- src/org/usfirst/frc/team1100/robot/commands/claw/PullCubeIn.java
+++ src/org/usfirst/frc/team1100/robot/commands/claw/PullCubeIn.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1100.robot.commands.claw;
 import org.usfirst.frc.team1100.robot.subsystems.Claw;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  *
@@ -10,31 +11,38 @@ import edu.wpi.first.wpilibj.command.Command;
 public class PullCubeIn extends Command {
 
     Claw claw;
+    Timer t;
+    double time;
     
     public PullCubeIn() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Claw.getInstance());
         claw = Claw.getInstance();
+        time = 10;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	claw.setPullSpeed(1);
+    	t = new Timer();
+    	t.start();
+    	claw.pullIn();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	claw.pullIn();
+    	// nothing to do but wait for time to expire
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return t.get() >= time;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	claw.pullStop();
     }
 
     // Called when another command which requires one or more of the same
